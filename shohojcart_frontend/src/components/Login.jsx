020f5de456389e085b412d5ac24e5cb83aafec1d
faiyazalam523 <faiyazwilles786@gmail.com>
2025-09-06 04:58:00 +0600

    feat: Enhance Login and Signup components with responsive design and form validation

--- shohojcart_frontend/src/components/Login.jsx
+++ shohojcart_frontend/src/components/Login.jsx
@@ -1,42 +1,89 @@
-import React from 'react'
-import './Login.css'
-import password_icon from '../assets/password.png'
-import email_icon from '../assets/email.png'
-import { useNavigate, useLocation } from 'react-router-dom'
+import React from "react";
+import "./Login.css";
+import password_icon from "../assets/password.png";
+import email_icon from "../assets/email.png";
+import { useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function Login() {
-    
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required"),
+  });
+
+  const handleSubmit = (values, { setSubmitting, resetForm }) => {
+    console.log("Login submitted:", values);
+
+    // Simulate login API
+    setTimeout(() => {
+      alert("Login successful!");
+      resetForm();
+      navigate("/dashboard"); // redirect to dashboard after login
+      setSubmitting(false);
+    }, 1000);
+  };
 
   return (
-    <div className='container'>
-        <div className="header">
-            <div className="text">Login</div>
-            <div className="underline"></div>
-        </div>
+    <div className="container">
+      <div className="header">
+        <div className="text">Login</div>
+        <div className="underline"></div>
+      </div>
 
-        <div className="inputs">
-            <div className="input">
-                <img src={email_icon} alt="" />
-                <input type="email" placeholder='Email'/>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div className="inputs">
+              <div className="input">
+                <img src={email_icon} alt="Email Icon" />
+                <Field type="email" name="email" placeholder="Email" />
+              </div>
+              <ErrorMessage name="email" component="div" className="error" />
+
+              <div className="input">
+                <img src={password_icon} alt="Password Icon" />
+                <Field type="password" name="password" placeholder="Password" />
+              </div>
+              <ErrorMessage name="password" component="div" className="error" />
             </div>
-             <div className="input">
-                <img src={password_icon} alt="" />
-                <input type="password" placeholder='Password'/>
+
+            <div className="forgot-password">
+              Lost password? <span onClick={() => navigate("/forgot")}>Click here!</span>
             </div>
-        </div>
 
-        <div className="forgot-password">
-            Lost password? <span>Click here!</span>
-        </div>
+            <div className="submit-container">
+              <button
+                type="button"
+                className="submit signup"
+                onClick={() => navigate("/signup")}
+              >
+                Sign Up
+              </button>
 
-        <div className="submit-container">
-            <div className={location.pathname === "/signup" ? "submit gray" : "submit"} onClick={() => navigate("/signup")}>Sign Up</div>
-            <div className={location.pathname === "/login" ? "submit gray" : "submit"}>Login</div>
-        </div>
+              <button
+                type="submit"
+                className="submit"
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? "Logging in..." : "Login"}
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- shohojcart_frontend/src/components/Signup.jsx
+++ shohojcart_frontend/src/components/Signup.jsx
@@ -1,42 +1,95 @@
-import React from 'react'
-import './Signup.css'
-import user_icon from '../assets/person.png'
-import password_icon from '../assets/password.png'
-import email_icon from '../assets/email.png'
-import { useNavigate, useLocation } from 'react-router-dom'
+import React from "react";
+import "./Signup.css";
+import user_icon from "../assets/person.png";
+import password_icon from "../assets/password.png";
+import email_icon from "../assets/email.png";
+import { useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function Signup() {
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .min(2, "Name must be at least 2 characters")
+      .required("Name is required"),
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required"),
+  });
+
+  const handleSubmit = (values, { setSubmitting, resetForm }) => {
+    console.log("Signup submitted:", values);
+
+    // Simulate signup API
+    setTimeout(() => {
+      alert("Signup successful!");
+      resetForm();
+      navigate("/login"); // redirect to login after signup
+      setSubmitting(false);
+    }, 1000);
+  };
 
   return (
-    <div className='container'>
-        <div className="header">
-            <div className="text">Sign Up</div>
-            <div className="underline"></div>
-        </div>
+    <div className="container">
+      <div className="header">
+        <div className="text">Sign Up</div>
+        <div className="underline"></div>
+      </div>
 
-        <div className="inputs">
-            <div className="input">
-                <img src={user_icon} alt="" />
-                <input type="text" placeholder='Name'/>
-            </div>
-            <div className="input">
-                <img src={email_icon} alt="" />
-                <input type="email" placeholder='Email'/>
-            </div>
-            <div className="input">
-                <img src={password_icon} alt="" />
-                <input type="password" placeholder='Password'/>
-            </div>
-        </div>
+      <Formik
+        initialValues={{ name: "", email: "", password: "" }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div className="inputs">
+              <div className="input">
+                <img src={user_icon} alt="User Icon" />
+                <Field type="text" name="name" placeholder="Name" />
+              </div>
+              <ErrorMessage name="name" component="div" className="error" />
 
-        <div className="submit-container">
-            <div className={location.pathname === "/signup" ? "submit gray" : "submit"}>Sign Up</div>
-            <div className={location.pathname === "/login" ? "submit gray" : "submit"} onClick={() => navigate("/login")}>Login</div>
-        </div>
+              <div className="input">
+                <img src={email_icon} alt="Email Icon" />
+                <Field type="email" name="email" placeholder="Email" />
+              </div>
+              <ErrorMessage name="email" component="div" className="error" />
+
+              <div className="input">
+                <img src={password_icon} alt="Password Icon" />
+                <Field type="password" name="password" placeholder="Password" />
+              </div>
+              <ErrorMessage name="password" component="div" className="error" />
+            </div>
+
+            <div className="submit-container">
+              <button
+                type="submit"
+                className="submit"
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? "Signing Up..." : "Sign Up"}
+              </button>
+
+              <button
+                type="button"
+                className="submit login-btn"
+                onClick={() => navigate("/login")}
+              >
+                Login
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
