6b8b6261b5e05d7cfd0f56de95b68bff545d54f1
Mohammad Rafid Ahad <163350541+rafidahad@users.noreply.github.com>
2025-09-16 05:33:58 +0600

    old files removed

--- shohojcart/frontend/public/js/preventBack.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    if (sessionStorage.getItem('loggedIn') === 'true') {
-        window.history.pushState(null, '', window.location.href);
-        window.onpopstate = function () {
-            window.history.go(1);
-        };
-    }
-});
--- shohojcart/frontend/src/utils/cn.js
+++
@@ -1,4 +0,0 @@
-export const cn = (...inputs) => {
-    return inputs.filter(Boolean).join(" ");
-  };
-  
