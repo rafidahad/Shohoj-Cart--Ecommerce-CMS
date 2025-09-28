ddb4d7e44618062826ec61c20a31b62325ddc8dd
Mohammad Rafid Ahad <163350541+rafidahad@users.noreply.github.com>
2025-09-28 17:37:38 +0600

    RENDER SETTINGS

--- shohojcart_backend/config/cors.php
+++ shohojcart_backend/config/cors.php
@@ -1,34 +1,23 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    'paths' => ['*'],
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => [env('FRONTEND_URL')],
-
+    
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'https://shohoj-cart-ecommerce-cms.onrender.com'),
+        'https://shohoj-cart-ecommerce-cms.onrender.com',
+        'http://localhost:5173', // for local development
+    ],
+    
     'allowed_origins_patterns' => [],
-
+    
     'allowed_headers' => ['*'],
-
+    
     'exposed_headers' => [],
-
+    
     'max_age' => 0,
-
+    
     'supports_credentials' => true,
-
 ];
